3f428b416cfb2bcf323a53a7493d5d592c29613d
me <me@mbp.home>
2015-09-07 03:04:58 -0400

    added checkactivity and a second timer to try making things easier.

--- deCATinator.ino
+++ deCATinator.ino
@@ -1,31 +1,35 @@
-#include "Timer.h";
+#include "Timer.h"
 
 // Declare Constants
+
 int zoneSensor[] = { 2, 3, 4, 5, 6 };
 int zoneValve[] = { 8, 9, 10, 11, 12 };
+//int sensorValveMap[] = {};
 
 int numZones = 5;
 
-const int ledPin = 13;             // Built-in LED
+const int ledPin =   13;             // Built-in LED
 const int ledBlinkTime = 500;      // Blink one for half a second while calibrating
 
+Timer t;
+Timer secondTimer;
 
 // Wait for the seonsor to calibrate (20 - 60 seconds according to datasheet)
 // 60 Seconds in milliseconds
-const unsigned int calibrationTime = 60000;
+const unsigned int calibrationTime = 120000;
 const unsigned int fifteenSecs = 5000;
 
 void setup() {
   Serial.begin(9600);
 
+  pinMode(ledPin, OUTPUT);
+  
   for (int currentPin = 0; currentPin < numZones; currentPin++) {
     pinMode(zoneSensor[currentPin], INPUT);
     pinMode(zoneValve[currentPin], OUTPUT);
-    
+//    sensorValveMap[zoneSensor[currentPin]] = zoneValve[currentPin];
   }
-  
 
-  
   // We need to wait one minute for the sensor to calibrate
   // Get out of view of the sensor for this duration!
   
@@ -37,7 +41,20 @@ void setup() {
     delay(ledBlinkTime);
   }
 
+  t.every(1000, checkActivity);
+
 }
+
+void checkActivity() {
+  for (int currentPin = 0; currentPin < numZones; currentPin++) {
+    if (sensorActive(zoneSensor[currentPin])) {
+      Serial.println(zoneSensor[currentPin]);
+//      openValve(sensorValveMap[zoneSensor[currentPin]], milliSecs, HIGH, 1);
+      openValve(zoneValve[currentPin], fifteenSecs);
+    }
+  }
+}
+
 
 bool sensorActive(int pinNumber) {
   if (digitalRead(pinNumber) == HIGH) {
@@ -46,24 +63,16 @@ bool sensorActive(int pinNumber) {
   return false;
 }
 
+
 void openValve(int pinNumber, int milliSecs) {
-  Timer t;
 
-  t.oscillate(pinNumber, milliSecs, HIGH, 1);
+  secondTimer.oscillate(pinNumber, milliSecs, HIGH, 1);
 }
 
-void updatePin(int currentPin) {
-  if(sensorActive(currentPin)) {
-    openValve(currentPin, fifteenSecs);
-  }
-}
 
 void loop() {
-  Timer bigT;
-  
-  for (int currentPin = 0; currentPin < numZones; currentPin++) {
-    bigT.every(1000, updatePin(currentPin));
-  }
+  t.update();
+  secondTimer.update();
 
 /*  // Constantly check the state of pin 2
   // If it is HIGH the sensor is detecting motion
